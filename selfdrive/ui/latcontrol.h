e024d5da2aa2aafc855e60c6f51f57f19f6c628f
Vehicle Researcher <>
2020-12-03 06:09:18 +0900

    openpilot v0.7.10 release

--- selfdrive/ui/latcontrol.h
+++ selfdrive/ui/latcontrol.h
@@ -114,7 +114,7 @@ static void draw_control_button3(UIState *s, int touch_x, int touch_y) {
       nvgStrokeColor(s->vg, nvgRGBA(55,184,104,255));
       nvgStrokeWidth(s->vg, 6);
       nvgStroke(s->vg);
-      nvgFillColor(s->vg, nvgRGBA(0,255,0,25));
+      nvgFillColor(s->vg, nvgRGBA(0,255,0,45));
       nvgFill(s->vg);
     } else {
       nvgStrokeColor(s->vg, nvgRGBA(255,255,255,80));
